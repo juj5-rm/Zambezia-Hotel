2373099a95b19b8b9b93b9b7196de5ef16791ae6
Daniel Rico <daniel.rico@utp.edu.co>
2024-05-12 10:09:14 -0500

    Move interfaces into front

---
+++ app/src/interfaces/hotel.interface.js
@@ -0,0 +1,22 @@
+export class User {
+  idUser; //int
+  passwordUser; //String
+  nameUser; //String
+  lastNameUser; //String
+  documentUser; //String
+  emailUser; //String
+  phoneNumberUser; //int
+  adressUser; //String
+  typeUser; //String
+  createdAt; //Date
+  updatedAt; //Date
+}
+
+export class typeRoom {
+  id; //int
+  nameTypeRoom; //String
+  maxCapacity; //int
+  price; //int
+  createdAt; //Date
+  updatedAt; //Date
+}
