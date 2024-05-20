5f5ed2ee81e5e75f23dd385ef36012c1edf22e49
Juan Jose Ruiz Mellizo <Juanjose.ruiz1@utp.edu.co>
2024-05-20 08:04:06 -0500

    hebitaciones

--- app/src/home-page/app.js
+++ app/src/home-page/app.js
@@ -9,24 +9,42 @@ console.log("Datos del usuario:", userData);
 // Espera a que el DOM esté completamente cargado
 document.addEventListener("DOMContentLoaded", function () {
   const servicios = document.querySelectorAll(".gridservicios .servicio");
+  const habitaciones = document.querySelectorAll(".gridservicios .habitacion");
   const descripciones = document.querySelectorAll(
     ".descripciones .serviciodescripcion"
   );
+  const descripcioneshabitaciones = document.querySelectorAll(
+    ".descripciones .habitaciondescripcion"
+  );
 
-  // Función para reiniciar colores y ocultar todas las descripciones
-  function resetAll() {
+  function resetservice() {
     servicios.forEach((servicio) => servicio.classList.remove("active"));
     descripciones.forEach(
       (descripcion) => (descripcion.style.display = "none")
+    );
+  }
+  // Función para reiniciar colores y ocultar todas las descripciones
+  function resetroom() {
+    habitaciones.forEach((room) => room.classList.remove("active"));
+    descripcioneshabitaciones.forEach(
+      (descripcionhabitacion) => (descripcionhabitacion.style.display = "none")
     );
   }
 
   // Añadir el evento de clic a cada servicio
   servicios.forEach((servicio, index) => {
     servicio.addEventListener("click", function () {
-      resetAll(); // Reiniciar todos los estilos y descripciones
+      resetservice(); // Reiniciar todos los estilos y descripciones
       this.classList.add("active"); // Añadir clase 'active' al servicio clickeado
       descripciones[index].style.display = "flex"; // Mostrar la descripción correspondiente
+    });
+  });
+
+  habitaciones.forEach((habitacion, index) => {
+    habitacion.addEventListener("click", function () {
+      resetroom(); // Reiniciar todos los estilos y descripciones
+      this.classList.add("active"); // Añadir clase 'active' al servicio clickeado
+      descripcioneshabitaciones[index].style.display = "flex"; // Mostrar la descripción correspondiente
     });
   });
 });
